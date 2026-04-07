95e093d86e8a3f4a499d7bbb4092b20cf350cb25
Effendi <effendi.saputra01@gmail.com>
2026-04-08 00:09:55 +0700

    adding some dynamic jump shit

--- player/script/main.gd
+++ player/script/main.gd
@@ -6,26 +6,22 @@ var PLAYER_STATE = state.PState.IDLE
 var PLAYER_DIR = state.Dir.RIGHT
 
 var PLAYER_VELOCITY = Vector2()
-const PLAYER_SPEED = 50
-const PLAYER_JUMP_MOD = -200
-
-var JUMP_BUFF = 3
+const PLAYER_SPEED = 30
+const PLAYER_SPEED_CAP = 100
+const PLAYER_JUMP_MOD = 200
+const PLAYER_FRICTION = 20
+var JUMP_BUFF = 1
 var INPUT_POLL = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	#Input.is_action_pressed("move_left")
-	
-#	pass # Replace with function body.
 
 func _physics_process(delta):
 	PLAYER_VELOCITY.y += Physics.GRAVITY * delta
 	process_input()
 	action()
 	PLAYER_VELOCITY = move_and_slide(PLAYER_VELOCITY, Vector2.UP)
-	
-	
+		
 func process_input():
 	if is_on_floor():
 		if Input.is_action_pressed("move_left"):
@@ -41,6 +37,12 @@ func process_input():
 	else:
 		PLAYER_STATE = state.PState.FALL
 		
+		if Input.is_action_pressed("move_left"):
+			PLAYER_DIR = state.Dir.LEFT
+		elif Input.is_action_pressed("move_right"):
+			PLAYER_DIR = state.Dir.RIGHT			
+		
+
 func action():
 	var speed = 0;
 	match PLAYER_DIR:
@@ -50,11 +52,18 @@ func action():
 			speed = PLAYER_SPEED
 	match PLAYER_STATE: 
 		state.PState.JUMP:
-			PLAYER_VELOCITY.y = PLAYER_JUMP_MOD * JUMP_BUFF
+			PLAYER_VELOCITY.y = -PLAYER_JUMP_MOD - (to_positive(PLAYER_VELOCITY.x) / 2) 
 		state.PState.RUN:
 			PLAYER_VELOCITY.x += speed
 		state.PState.IDLE:
-			PLAYER_VELOCITY.x = 0
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+			if PLAYER_VELOCITY.x > 0:
+				PLAYER_VELOCITY.x -= PLAYER_FRICTION
+			elif PLAYER_VELOCITY.x < 0:
+				PLAYER_VELOCITY.x += PLAYER_FRICTION
+
+
+func to_positive(number: int) -> int:
+	if number < 0: 
+		return number * -1
+	else:
+		return number
