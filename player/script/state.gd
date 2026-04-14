181c0b82d1b2a669240a0d9bb7c9a4d19841c80c
Effendi <effendi.saputra01@gmail.com>
2026-04-14 22:39:36 +0700

    actually making this shit more structured i suppose...

--- player/script/state.gd
+++ player/script/state.gd
@@ -2,23 +2,53 @@
 const PLAYER_SPEED = 200
 const JUMP_MOD = -200
 
-
 enum Action {
 	RUN,
-	RUN_STARTUP,
 	IDLE,
 	JUMP,
 	SLIDE,
 	FALL,
+	LANDING,
+	CROUCH,
 }
 
 # Jadi value disini korespon terhadap "lamanya" aksi itu nanti, per frame based
-const Action_Frame = {
-	Action.RUN: 1,
-	Action.IDLE: 1,
-	Action.JUMP: 60,
-	Action.FALL: 40,
-	Action.SLIDE: 40
+const Action_Pack = {
+	Action.RUN: {
+		"Frame": 1,
+		"Floor": Action.IDLE,
+		"Float": Action.FALL
+	},
+	Action.CROUCH: {
+		"Frame": 1,
+		"Floor": Action.IDLE,
+		"Float": Action.FALL
+	},
+	Action.IDLE: {
+		"Frame": 1,
+		"Floor": Action.IDLE,
+		"Float": Action.FALL
+	},
+	Action.JUMP: {
+		"Frame": 60,
+		"Floor": Action.LANDING,
+		"Float": Action.FALL
+	},
+	Action.FALL: {
+		"Frame": 1,
+		"Floor": Action.LANDING,
+		"Float": Action.FALL
+	},
+	Action.LANDING: {
+		"Frame": 25,
+		"Floor": Action.IDLE,
+		"Float": Action.FALL
+	},
+	Action.SLIDE: {
+		"Frame": 40,
+		"Floor": Action.IDLE,
+		"Float": Action.FALL
+	}
 }
 
 enum Dir {
